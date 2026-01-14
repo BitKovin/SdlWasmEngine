db556577f4588c174062defb3b6f92563e390e9a
BitKovin <39824091+BitKovin@users.noreply.github.com>
2026-01-14 14:48:12 +0200

    Update DetourMath.h

--- source/Engine/Navigation/Detour/DetourMath.h
+++ source/Engine/Navigation/Detour/DetourMath.h
@@ -9,13 +9,13 @@ Members in this module are wrappers around the standard math library
 
 #include <cmath>
 
-inline float dtMathFabsf(float x) { return ::fabsf(x); }
-inline float dtMathSqrtf(float x) { return ::sqrtf(x); }
-inline float dtMathFloorf(float x) { return ::floorf(x); }
-inline float dtMathCeilf(float x) { return ::ceilf(x); }
-inline float dtMathCosf(float x) { return ::cosf(x); }
-inline float dtMathSinf(float x) { return ::sinf(x); }
-inline float dtMathAtan2f(float y, float x) { return ::atan2f(y, x); }
-inline bool dtMathIsfinite(float x) { return ::isfinite(x); }
+inline float dtMathFabsf(float x) { return std::fabsf(x); }
+inline float dtMathSqrtf(float x) { return std::sqrtf(x); }
+inline float dtMathFloorf(float x) { return std::floorf(x); }
+inline float dtMathCeilf(float x) { return std::ceilf(x); }
+inline float dtMathCosf(float x) { return std::cosf(x); }
+inline float dtMathSinf(float x) { return std::sinf(x); }
+inline float dtMathAtan2f(float y, float x) { return std::atan2f(y, x); }
+inline bool dtMathIsfinite(float x) { return std::isfinite(x); }
 
 #endif
