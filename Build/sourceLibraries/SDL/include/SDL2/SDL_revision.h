1c9cdd1c0fa51931255f9fc0292907a21d6c472d
BitKovin <39824091+BitKovin@users.noreply.github.com>
2026-03-10 01:19:31 +0200

    improved cmake

--- Build/sourceLibraries/SDL/include/SDL2/SDL_revision.h
+++ Build/sourceLibraries/SDL/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.32.10-gebac9b8a (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.32.10-g73884ce3 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.32.10-gebac9b8a"
+#define SDL_REVISION "SDL-2.32.10-g73884ce3"
 #endif
