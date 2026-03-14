71e1155b33e4854af601808e67345e416696dfe4
BitKovin <39824091+BitKovin@users.noreply.github.com>
2026-03-14 18:51:28 +0200

    small fixes

--- Build/sourceLibraries/JoltPhysics-5.3.0/Build/JoltConfig.cmake
+++ Build/sourceLibraries/JoltPhysics-5.3.0/Build/JoltConfig.cmake
@@ -56,11 +56,11 @@ set_target_properties(Jolt::Jolt PROPERTIES
   INTERFACE_LINK_OPTIONS "-Wno-limited-postlink-optimizations"
 )
 
-# Import target "Jolt::Jolt" for configuration ""
-set_property(TARGET Jolt::Jolt APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+# Import target "Jolt::Jolt" for configuration "Release"
+set_property(TARGET Jolt::Jolt APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(Jolt::Jolt PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
-  IMPORTED_LOCATION_NOCONFIG "F:/Git/SdlWasmEngine/Build/sourceLibraries/JoltPhysics-5.3.0/Build/libJolt.a"
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "F:/Git/SdlWasmEngine/Build/sourceLibraries/JoltPhysics-5.3.0/Build/libJolt.a"
   )
 
 # This file does not depend on other imported targets which have
--- Build/sourceLibraries/assimp/include/assimp/revision.h
+++ Build/sourceLibraries/assimp/include/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x1f3148f2
+#define GitVersion 0x8944456f
 #define GitBranch "bgfx"
 
 #define VER_MAJOR 6
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 1f3148f2)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 8944456f)"
 #endif
 #define VER_COPYRIGHT_STR           "\xA9 2006-2023"
 
