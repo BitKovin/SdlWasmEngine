2c994f10775c4cc4d3e4340d4b3ec119abf0e5a2
BitKovin <39824091+BitKovin@users.noreply.github.com>
2026-03-14 21:02:55 +0200

    fixed npc human axe crashing game from parry damage

--- Build/sourceLibraries/assimp/include/assimp/revision.h
+++ Build/sourceLibraries/assimp/include/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x71e1155b
+#define GitVersion 0x55677e2c
 #define GitBranch "bgfx"
 
 #define VER_MAJOR 6
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 71e1155b)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 55677e2c)"
 #endif
 #define VER_COPYRIGHT_STR           "\xA9 2006-2023"
 
