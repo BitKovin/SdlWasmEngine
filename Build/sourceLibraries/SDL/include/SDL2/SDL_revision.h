38949ba3df1cbe311fcc8ed74814952e915e83c1
BitKovin <39824091+BitKovin@users.noreply.github.com>
2026-03-18 19:34:32 +0000

    upd

--- Build/sourceLibraries/SDL/include/SDL2/SDL_revision.h
+++ Build/sourceLibraries/SDL/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.32.10-gaee8cf0b (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.32.10-gb55b35f7 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.32.10-gaee8cf0b"
+#define SDL_REVISION "SDL-2.32.10-gb55b35f7"
 #endif
--- Build/sourceLibraries/assimp/include/assimp/revision.h
+++ Build/sourceLibraries/assimp/include/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xaee8cf0b
+#define GitVersion 0xb55b35f7
 #define GitBranch "bgfx"
 
 #define VER_MAJOR 6
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit aee8cf0b)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit b55b35f7)"
 #endif
 #define VER_COPYRIGHT_STR           "\xA9 2006-2023"
 
